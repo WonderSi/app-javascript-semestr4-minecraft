1d8f936ea74741a197d4711b2019be2a3eb6ce18
WonderSi <kirillkozlov00@gmail.com>
2025-05-17 15:38:06 +0700

    create Logger.js

---
+++ js/utils/Logger.js
@@ -0,0 +1,24 @@
+class Logger {
+  constructor(moduleName) {
+    this.moduleName = moduleName;
+  }
+
+  log(message) {
+    console.log(
+      `[${new Date().toISOString()}] [${this.moduleName}] [INFO] ${message}`
+    );
+  }
+
+  error(message, error) {
+    console.error(
+      `[${new Date().toISOString()}] [${this.moduleName}] [ERROR] ${message}`,
+      error
+    );
+  }
+
+  warn(message) {
+    console.warn(
+      `[${new Date().toISOString()}] [${this.moduleName}] [WARN] ${message}`
+    );
+  }
+}
