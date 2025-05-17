3450b38aee06b48f937c81bb99f50f70c519c8f7
WonderSi <kirillkozlov00@gmail.com>
2025-05-17 16:01:51 +0700

    added log in MainView.js

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -1,9 +1,15 @@
+import Logger from "../utils/Logger.js";
+
 class MainView {
   constructor() {
     this.app = document.getElementById("app");
+
+    this.logger = new Logger('MainView')
+    this.logger.log("MainView инициализированный")
   }
 
   render(username) {
+    this.logger.log('render')
     this.app.innerHTML = `
     <div id="main-container">
         <header id="main-header">
@@ -17,10 +23,10 @@ class MainView {
         </header>
     </div>
     `;
-    console.log("renderMain");
   }
 
   bindLogout(handler) {
+    this.logger.log('bindLogout')
     document.getElementById('main-logout-btn').addEventListener('click', handler)
   }
 }
