02e3d4a67abac92c200e6d705f4e726a18da87be
WonderSi <kirillkozlov00@gmail.com>
2025-05-17 17:57:49 +0700

    filtering and alight style
    added a little filtering and a light style.

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -4,12 +4,12 @@ class MainView {
   constructor() {
     this.app = document.getElementById("app");
 
-    this.logger = new Logger('MainView')
-    this.logger.log("MainView инициализированный")
+    this.logger = new Logger("MainView");
+    this.logger.log("MainView инициализированный");
   }
 
   render(username) {
-    this.logger.log('render')
+    this.logger.log("render");
     this.app.innerHTML = `
     <div id="main-container">
         <header id="main-header">
@@ -21,13 +21,43 @@ class MainView {
                 <button id="main-logout-btn">ВЫЙТИ</button>
             </dev>
         </header>
+
+        <main id="main-content">
+          <div id="main-sidebar">
+            <div id="main-filter-section">
+              <div>
+                <div id="-main-filter-search">
+                  <input type="text" id="main-search-input" placeholder="Введите название предмета">
+                </div>
+                <div id="main-filter-sort">
+                  <select id="sort-select">
+                    <option value="name-asc">По имени (А-Я)</option>
+                    <option value="name-desc">По имени (Я-А)</option>
+                    <option value="stackSize-asc">По размеру стака (возр.)</option>
+                    <option value="stackSize-desc">По размеру стака (убыв.)</option>
+                  </select>
+                </div>
+                <div id="main-filter-group">
+                  <label>
+                    <input type="checkbox" id="main-renewable-filter-input"> Возобновляемые
+                  </label>
+                  <label>
+                    <input type="checkbox" id="main-favorites-filter-input"> Избранное
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
     </div>
     `;
   }
 
   bindLogout(handler) {
-    this.logger.log('bindLogout')
-    document.getElementById('main-logout-btn').addEventListener('click', handler)
+    this.logger.log("bindLogout");
+    document
+      .getElementById("main-logout-btn")
+      .addEventListener("click", handler);
   }
 }
 
