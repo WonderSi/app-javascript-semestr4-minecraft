17dfc0adcd1f3a24ee555a3972d390d59b055198
WonderSi <kirillkozlov00@gmail.com>
2025-05-23 18:52:59 +0700

    creating a card style

--- js/views/ItemListView.js
+++ js/views/ItemListView.js
@@ -20,20 +20,25 @@ class ItemListView {
 
     items.forEach((item) => {
       const itemCard = document.createElement("div");
+      itemCard.className = "item-card";
+      itemCard.dataset.id = item.namespacedId;
       itemCard.innerHTML = `
-            <div class="item-image">
+
+            <div id="item-image">
                 <img src="${item.image}" alt="${item.name}">
             </div>
-            <div class="item-info">
+            <div id="item-info">
                 <h3>${item.name}</h3>
-                <p class="item-stack-size">Стек: ${item.stackSize}</p>
-                <p class="item-renewable">${
+                <p id="item-stack-size">Стек: ${item.stackSize}</p>
+                <p id="item-renewable">${
                   item.renewable ? "Возобновляемый" : "Невозобновляемый"
                 }</p>
             </div>
-            <div class="item-actions">
-
+            <div id="item-actions">
+                <button id="item-detail-btn">Подробнее</button>
+                <button id="item-favorite-btn"><img src="../../assets/svg/star_full.svg"></button>
             </div>
+            
         `;
       this.container.appendChild(itemCard);
     });
